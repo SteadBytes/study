da5a833c098e6701e054ac0fa9df85ec95de26ba
Ben Steadman <steadmanben1@gmail.com>
2019-08-18 07:14:54 +0100

    initial Person model & test data generator

---
+++ tpp-16/address_book/models.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass, field
+from typing import Iterable
+from uuid import uuid4
+
+from faker import Faker
+
+fake = Faker()
+
+
+@dataclass
+class Person:
+    first_name: str
+    last_name: str
+    phone_number: str
+    house_number: str
+    street: str
+    town: str
+    postcode: str
+    id: str = field(default_factory=lambda: str(uuid4()))
+
+
+def generate_people(n: int) -> Iterable[Person]:
+    for _ in range(n):
+        yield Person(
+            fake.first_name(),
+            fake.last_name(),
+            fake.phone_number(),
+            fake.building_number(),
+            fake.street_name(),
+            fake.city(),
+            fake.postalcode(),
+        )
