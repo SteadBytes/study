3f81dc77813bf5a467cc3f5c0bb8bfdd911b97e5
Ben Steadman <steadmanben1@gmail.com>
2019-08-18 06:43:41 +0100

    tpp-14-ex7

---
+++ tpp-14-ex7/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to tpp-14-ex7
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ tpp-14-ex7/test/tpp_14_ex7/core_test.clj
@@ -0,0 +1,7 @@
+(ns tpp-14-ex7.core-test
+  (:require [clojure.test :refer :all]
+            [tpp-14-ex7.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
