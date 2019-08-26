acfc99834556704fc892f3304176df31c0f3d675
Ben Steadman <steadmanben1@gmail.com>
2019-08-26 10:29:44 +0100

    plain_text tests & tidy up

--- tpp-16/tests/test_address_book.py
+++ tpp-16/tests/test_address_book.py
@@ -1,31 +1,37 @@
 import pytest
 
-from address_book import binary
+from address_book import binary, plain_text
 from address_book.models import Person, generate_people
 
 
-@pytest.fixture
-def people_list():
-    return list(generate_people(50))
+@pytest.mark.parametrize("p", generate_people(50))
+def test_to_bytes_inverts_from_bytes(p):
+    p_bytes, size = binary.to_bytes(p)
+    p_again = binary.from_bytes(p_bytes)
+    assert p == p_again
 
 
-class TestBinary:
-    @pytest.mark.parametrize("p", generate_people(50))
-    def test_to_bytes_inverts_from_bytes(self, p):
-        p_bytes, size = binary.to_bytes(p)
-        p_again = binary.from_bytes(p_bytes)
-        assert p == p_again
+@pytest.mark.parametrize("p", generate_people(50))
+def test_to_dict_inverts_from_dict(p):
+    p_dict = plain_text.to_dict(p)
+    p_again = plain_text.from_dict(p_dict)
+    assert p == p_again
 
-    def test_write_address_book_inverts_read_address_book(self, tmp_path, people_list):
-        db = tmp_path / "address-book.bin"
-        # sanity check
-        assert db.exists() is False
 
-        binary.write_address_book(db, people_list)
+@pytest.mark.parametrize(
+    "module,fname", [(binary, "address-book.bin"), (plain_text, "address-book.json")]
+)
+def test_write_address_book_inverts_read_address_book(module, fname, tmp_path):
+    db = tmp_path / fname
+    # sanity check
+    assert db.exists() is False
 
-        assert db.exists() is True
-        assert db.stat().st_size > 0
+    people = list(generate_people(50))
+    module.write_address_book(db, people)
 
-        people_list_again = binary.read_address_book(db)
+    assert db.exists() is True
+    assert db.stat().st_size > 0
 
-        assert people_list == people_list_again
+    people_again = module.read_address_book(db)
+
+    assert people == people_again
