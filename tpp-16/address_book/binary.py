5d322b9d7927b5c6b3d1c51acc37fc25281d48ee
Ben Steadman <steadmanben1@gmail.com>
2019-08-26 08:27:39 +0100

    prepend binary records with size

--- tpp-16/address_book/binary.py
+++ tpp-16/address_book/binary.py
@@ -2,9 +2,9 @@ import struct
 from dataclasses import astuple
 from functools import partial
 from pathlib import Path
-from typing import Iterable
+from typing import Iterable, Tuple
 
-from models import Person
+from .models import Person
 
 PERSON_STRUCT_FMT = "50s50s30s10s50s50s10s{}s36s"
 
@@ -31,20 +31,34 @@ def from_bytes(buffer: bytes) -> Person:
     return Person(*(x.decode("utf-8").rstrip("\x00") for x in data))
 
 
-def to_bytes(p: Person) -> bytes:
+def to_bytes(p: Person) -> Tuple[bytes, int]:
     # dynamically add size to format for variable length directions field
     fmt = PERSON_STRUCT_FMT.format(len(p.directions))
-    return struct.pack(fmt, *(s.encode("utf-8") for s in astuple(p)))
+    return (
+        struct.pack(fmt, *(s.encode("utf-8") for s in astuple(p))),
+        struct.calcsize(fmt),
+    )
+
+
+RecordSizeStruct = struct.Struct("I")
 
 
 def read_address_book(db: Path):
     people = []
     with db.open("rb") as f:
-        for chunk in iter(partial(f.read, PersonStruct.size), b""):
-            people.append(from_bytes(chunk))
+        while True:
+            size_buf = f.read(RecordSizeStruct.size)
+            if not size_buf:
+                break
+            record_size = RecordSizeStruct.unpack(size_buf)[0]
+            people.append(from_bytes(f.read(record_size)))
     return people
 
 
 def write_address_book(db: Path, people: Iterable[Person]):
     with db.open("wb") as f:
-        f.write(b"".join(to_bytes(p) for p in people))
+        records_with_sizes = (
+            RecordSizeStruct.pack(size) + p_bytes
+            for p_bytes, size in (to_bytes(p) for p in people)
+        )
+        f.write(b"".join(records_with_sizes))
