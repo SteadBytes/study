91ac795fe9c93edf9e9694b9b673193813fb09c5
Ben Steadman <steadmanben1@gmail.com>
2019-09-22 15:50:53 +0100

    improved read_list using linked list

--- blog/c-read-file-optimisation/main.c
+++ blog/c-read-file-optimisation/main.c
@@ -39,3 +39,56 @@ size_t read_file(int fd, char **data)
     }
     return len;
 }
+
+typedef struct node_t
+{
+    char *data;
+    int len;
+    struct node_t *next;
+} node_t;
+
+size_t read_file_improved(int fd, char **data)
+{
+    char buf[BUFSIZE];
+    ssize_t read_len;
+    size_t len = 0;
+
+    /* build linked list containing chunks of data read from fd */
+    node_t *head = NULL;
+    node_t *prev = NULL;
+    while ((read_len = read(fd, buf, BUFSIZE)) > 0)
+    {
+        node_t *node = malloc(sizeof(node_t));
+        node->data = malloc(read_len);
+        memcpy(node->data, buf, read_len);
+        node->len = read_len;
+        node->next = NULL;
+
+        /* first chunk */
+        if (head == NULL)
+        {
+            head = node;
+        }
+        /* second chunk onwards */
+        if (prev != NULL)
+        {
+            prev->next = node;
+        }
+        prev = node;
+        len += read_len;
+    }
+
+    /* copy each chunk into data once only */
+    *data = malloc(len);
+    char *p = *data;
+    node_t *cur = head;
+    while (cur != NULL)
+    {
+        memcpy(p, cur->data, cur->len);
+        p += cur->len;
+        cur = cur->next;
+    }
+
+    return len;
+}
+
