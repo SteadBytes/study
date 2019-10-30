e5740ee2d39da50c72737ab635fe1b84d725986b
Ben Steadman <steadmanben1@gmail.com>
2019-10-30 20:07:18 +0000

    [CodeKata] WIP 05 calculate error rates e.t.c

--- general-exercises/kata/codekata/05-bloom-filters/bloom.py
+++ general-exercises/kata/codekata/05-bloom-filters/bloom.py
@@ -1,11 +1,6 @@
+import math
 from hashlib import md5
-from itertools import zip_longest
-
-N_HASHES = 8
-
-
-def bitmap(m: int) -> list:
-    return [0 for _ in range(m)]
+from itertools import zip_longest, islice
 
 
 def grouper(iterable, n, fillvalue=None):
@@ -15,41 +10,58 @@ def grouper(iterable, n, fillvalue=None):
     return zip_longest(*args, fillvalue=fillvalue)
 
 
-def hashes(x: str, n: int):
-    h = md5(x.encode("utf-8")).hexdigest()
-    return ("".join(_h) for _h in grouper(h, len(h) // n))
+def hashes(data: bytes, k: int):
+    h = md5(data).hexdigest()
+    n_hashes = math.ceil(len(h) // k)
+    return ("".join(_h) for _h in islice(grouper(h, n_hashes), 0, n_hashes + 1))
 
 
-def hash_positions(x: str, m: int):
-    for h in hashes(x, N_HASHES):
+def hash_positions(data: bytes, k: int, m: int):
+    for h in hashes(data, k):
         yield int(h, 16) % m
 
 
-def insert(bm: list, *xs: str):
-    for x in xs:
-        for p in hash_positions(x, len(bm)):
-            bm[p] |= 1
+def error_rate(k, n, m):
+    return math.pow(1 - math.pow(math.e, (-k * n) / m), k)
 
 
-def test(bm, x):
-    vals = [bm[p] for p in hash_positions(x, len(bm))]
-    return all(vals)
+def calculate_m_k(n, target_error_rate, start_m=100):
+    # TODO: Stop this loop after a sensible amount of iterations
+    m = start_m
+    while True:
+        k = math.ceil((m / n) * math.log(2))
+        if error_rate(k, n, m) <= target_error_rate:
+            return m, k
+        else:
+            m *= 2
 
 
-def bloom_filter(m: int, iterable=None):
-    bm = bitmap(m)
-    if iterable:
-        insert(bm, *iterable)
-    return bm
+class BloomFilter:
+    def __init__(self, n: int, iterable=None, error_rate=0.01):
+        # TODO: check values of m and k are reasonable?
+        self.m, self.k = calculate_m_k(n, target_error_rate=error_rate)
+        self.bm = [0 for _ in range(self.m)]
+
+        if iterable:
+            self.add(*iterable)
+
+    def add(self, *vals: str):
+        for v in vals:
+            for p in self._hash_positions(v):
+                self.bm[p] |= 1
+
+    def _hash_positions(self, val: str):
+        return hash_positions(val.encode("utf-8"), self.k, self.m)
+
+    def __contains__(self, val: str):
+        return all(self.bm[p] for p in self._hash_positions(val))
 
 
 if __name__ == "__main__":
-    from pathlib import Path
+    with open("/usr/share/dict/words") as f:
+        words = [l.strip() for l in f]
+        bm = BloomFilter(len(words), iterable=words)
 
-    words = Path("/usr/share/dict/words")
-    with words.open() as f:
-        bm = bloom_filter(1000000, f)
-
-    print(test(bm, "hello"))
-    print(test(bm, ";alksdjf"))
-    print(test(bm, "llk"))
+    # TODO: FIX THIS
+    print("hello" in bm)
+    print("elloh" in bm)
