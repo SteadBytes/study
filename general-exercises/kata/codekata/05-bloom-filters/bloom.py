d6ed64008e67173082bca21c042a0660243746c8
Ben Steadman <steadmanben1@gmail.com>
2019-10-31 07:50:22 +0000

    [CodeKata] WIP 05 properly calculate multiple hashes

--- general-exercises/kata/codekata/05-bloom-filters/bloom.py
+++ general-exercises/kata/codekata/05-bloom-filters/bloom.py
@@ -1,45 +1,56 @@
+import hashlib
 import math
-from hashlib import md5
-from itertools import zip_longest, islice
+from struct import unpack
+
+FMT_SIZES = {"Q": 8, "I": 4, "H": 2}
 
 
-def grouper(iterable, n, fillvalue=None):
-    "Collect data into fixed-length chunks or blocks"
-    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
-    args = [iter(iterable)] * n
-    return zip_longest(*args, fillvalue=fillvalue)
+def _choose_hash(hash_bits: int):
+    # TODO: document
+    # TODO: check these values
+    # TODO: non-cryptographic hashes for perf improvement?
+    if hash_bits > 384:
+        return hashlib.sha512
+    elif hash_bits > 256:
+        return hashlib.sha384
+    elif hash_bits > 160:
+        return hashlib.sha256
+    elif hash_bits > 128:
+        return hashlib.sha1
+    else:
+        return hashlib.md5
 
 
-def hashes(data: bytes, k: int):
-    h = md5(data).hexdigest()
-    n_hashes = math.ceil(len(h) // k)
-    return ("".join(_h) for _h in islice(grouper(h, n_hashes), 0, n_hashes + 1))
+def _make_hashes_f(k, m):
+    # TODO: document
+    # TODO: check these values
+    if m >= (1 << 31):
+        fmt_char = "Q`"
+    elif m >= (1 << 15):
+        fmt_char = "I"
+    else:
+        fmt_char = "H"
 
+    hash_bits = 8 * k * FMT_SIZES[fmt_char]
+    _hash_f = _choose_hash(hash_bits)
+    fmt = fmt_char * (_hash_f().digest_size // FMT_SIZES[fmt_char])
 
-def hash_positions(data: bytes, k: int, m: int):
-    for h in hashes(data, k):
-        yield int(h, 16) % m
+    def _hashes_f(data: bytes):
+        h = _hash_f(data)
+        for _int in unpack(fmt, h.digest())[:k]:
+            yield _int % m
 
-
-def error_rate(k, n, m):
-    return math.pow(1 - math.pow(math.e, (-k * n) / m), k)
-
-
-def calculate_m_k(n, target_error_rate, start_m=100):
-    # TODO: Stop this loop after a sensible amount of iterations
-    m = start_m
-    while True:
-        k = math.ceil((m / n) * math.log(2))
-        if error_rate(k, n, m) <= target_error_rate:
-            return m, k
-        else:
-            m *= 2
+    return _hashes_f
 
 
 class BloomFilter:
     def __init__(self, n: int, iterable=None, error_rate=0.01):
-        # TODO: check values of m and k are reasonable?
-        self.m, self.k = calculate_m_k(n, target_error_rate=error_rate)
+        # TODO: rename n, m, k & document var names for comparison to literature
+        # TODO: formulas
+        self.m = math.ceil(-n * math.log(error_rate) / (math.log(2) ** 2))
+        self.k = math.ceil(self.m / n * math.log(2))
+        self._hashes_f = _make_hashes_f(self.k, self.m)
+        # TODO: more efficient bitmap representation
         self.bm = [0 for _ in range(self.m)]
 
         if iterable:
@@ -51,7 +62,7 @@ class BloomFilter:
                 self.bm[p] |= 1
 
     def _hash_positions(self, val: str):
-        return hash_positions(val.encode("utf-8"), self.k, self.m)
+        yield from self._hashes_f(val.encode("utf-8"))
 
     def __contains__(self, val: str):
         return all(self.bm[p] for p in self._hash_positions(val))
@@ -60,8 +71,8 @@ class BloomFilter:
 if __name__ == "__main__":
     with open("/usr/share/dict/words") as f:
         words = [l.strip() for l in f]
-        bm = BloomFilter(len(words), iterable=words)
+        bf = BloomFilter(len(words), iterable=words)
 
-    # TODO: FIX THIS
-    print("hello" in bm)
-    print("elloh" in bm)
+    print("hello" in bf)
+    print("test" in bf)
+    print("ljasdkf" in bf)
