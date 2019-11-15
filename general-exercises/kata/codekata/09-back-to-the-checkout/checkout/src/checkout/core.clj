2923f59b333d62caef3e20d68c0c348489f4cedf
Ben Steadman <steadmanben1@gmail.com>
2019-11-15 07:11:13 +0000

    [CodeKata] 09 initial re-design specs

--- general-exercises/kata/codekata/09-back-to-the-checkout/checkout/src/checkout/core.clj
+++ general-exercises/kata/codekata/09-back-to-the-checkout/checkout/src/checkout/core.clj
@@ -1,37 +1,22 @@
 (ns checkout.core
+  (:require [clojure.spec.alpha :as s])
   (:gen-class))
 
-(defn multi-price [{n :n price :price}]
-  (fn [n-items unit-price]
-    (let [n-unit (rem n-items n)
-          n-multi (/ (- n-items n-unit) n)]
-      (+ (* n-multi price) (* n-unit unit-price)))))
-
-(defn calc-total
-  [items pricing-rules]
-  (reduce + (map
-             (fn [[item n]]
-               (let [special-price (get-in pricing-rules [item :special-price])
-                     unit-price (get-in pricing-rules [item :unit-price])]
-                 (if special-price
-                   ((multi-price special-price) n unit-price)
-                   (* n unit-price))))
-             (frequencies items))))
-
-(defn add-total
-  [{items :items, pricing-rules :pricing-rules, :as co}]
-  (assoc co :total (calc-total items pricing-rules)))
-
-(defn scan
-  [co item]
-  (add-total (update co :items  #(conj % item))))
-
-(defn checkout [items pricing-rules]
-  (add-total {:items items :pricing-rules pricing-rules}))
+(s/def :rule/type keyword?)
+(defmulti rule-type :rule/type)
+(defmethod rule-type :per
+  "continuous pricing; rate of n per price"
+  [_]
+  (s/keys :req [::n ::price]))
+(defmethod rule-type :for
+  "n items for price-1, with remaining at price-2 each"
+  [_]
+  (s/keys :req [::n ::price-1 ::price-2]))
+(s/def ::pricing-rule (s/multi-spec rule-type :rule/type))
+(s/def ::name string?)
+(s/def ::quantity int?)
+(s/def ::product (s/keys :req [::name ::quantity ::pricing-rule]))
 
 (comment
-  (def pricing-rules {"A" {:unit-price 50 :special-price {:n 3 :price 130}}
-                      "B" {:unit-price 30 :special-price {:n 2 :price 45}}
-                      "C" {:unit-price 20}
-                      "D" {:unit-price 15}})
-  (:total (checkout ["B" "A" "B"] pricing-rules)))
+  (s/explain ::pricing-rule {:rule/type :per ::n 1 ::price 10})
+  (s/explain ::pricing-rule {:rule/type :for ::n 1 ::price-1 10 ::price-2 5}))
