4244bc52710d6feed4c85ec34188aac663fd8985
Ben Steadman <steadmanben1@gmail.com>
2020-05-08 14:53:33 +0100

    [CodeKata] 18 Iterator implementation.
    Return an Iterator of dependencies instead of eagerly populating
    a vector. This allows the caller more flexibility, makes the
    implementation cleaner and enables usage on much larger graphs where one
    may not need to generate the *entire* dependency list.
    
    Signed-off-by: Ben Steadman <steadmanben1@gmail.com>

--- general-exercises/kata/codekata/18-transitive-dependencies/src/lib.rs
+++ general-exercises/kata/codekata/18-transitive-dependencies/src/lib.rs
@@ -1,6 +1,9 @@
 #![allow(dead_code)] // Code is only used within test module
 
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::{
+    collections::{HashMap, HashSet, VecDeque},
+    iter::from_fn,
+};
 
 struct Dependencies<'a> {
     map: HashMap<&'a str, Vec<&'a str>>,
@@ -22,11 +25,15 @@ impl<'a> Dependencies<'a> {
     /// not present in the dependency graph. This is *not* a topological
     /// ordering; cyclic dependencies are handled as only the *existence* of
     /// the dependency is required, not some resolution order.
-    // TODO: Re-implement using Iterator
-    pub fn dependencies_for(&self, src: &'a str) -> Option<Vec<&'a str>> {
+    pub fn dependencies_for(&self, src: &'a str) -> Option<impl Iterator<Item = &str>> {
         if !self.map.contains_key(src) {
             return None;
         };
+
+        let mut frontier = VecDeque::new();
+        let mut visited = HashSet::new();
+        frontier.push_back(src);
+        visited.insert(src);
 
         // Perform a bread-first search of the graph starting from `src`.
         // Each dependency need only be included in the result once. To achieve
@@ -35,43 +42,38 @@ impl<'a> Dependencies<'a> {
         // searched in a previous iteration. This also prevents infinite loops
         // in the case of cyclic dependencies.
 
-        let mut deps = Vec::new();
-        let mut visited = HashSet::new();
-        let mut frontier = VecDeque::new();
-
-        // Start the search at `src`
-        visited.insert(src);
-        frontier.push_back(src);
-
         // TODO: Reduce nesting
-        while let Some(v) = frontier.pop_front() {
-            if let Some(next_deps) = self.map.get(v) {
-                for dep in next_deps {
-                    if !visited.contains(dep) {
-                        visited.insert(dep);
-                        frontier.push_back(dep);
+        let deps_iter = from_fn(move || {
+            if let Some(v) = frontier.pop_front() {
+                if let Some(next_deps) = self.map.get(v) {
+                    for dep in next_deps {
+                        if !visited.contains(dep) {
+                            visited.insert(dep);
+                            frontier.push_back(dep);
+                        }
                     }
                 }
+                Some(v)
+            } else {
+                None
             }
-            // TODO: Find a way to avoid this. Perhaps implement this with an
-            // Iterator instead of returning a Vec and then skip(1)?
-            if v != src {
-                deps.push(v);
-            }
-        }
-        Some(deps)
+        });
+
+        // Don't include `src` as the first item in the dependencies.
+        Some(deps_iter.skip(1))
     }
 }
 
 #[cfg(test)]
 mod tests {
 
-    macro_rules! assert_contents_eq {
+    macro_rules! assert_ok_contents_eq {
         ($left:expr, $right:expr) => {{
-            match (&$left, &$right) {
+            match ($left, $right) {
                 (left_val, right_val) => {
-                    let mut a: Vec<_> = left_val.iter().collect();
-                    let mut b: Vec<_> = right_val.iter().collect();
+                    let mut a: Vec<_> = left_val.unwrap().collect();
+                    let mut b: Vec<_> = right_val.iter().cloned().collect();
+
                     a.sort();
                     b.sort();
 
@@ -98,21 +100,17 @@ mod tests {
         dep.add_direct("E", &["F"]);
         dep.add_direct("F", &["H"]);
 
-        assert_contents_eq!(
-            dep.dependencies_for("A").unwrap(),
-            ["B", "C", "E", "F", "G", "H"]
-        );
-        assert_contents_eq!(
-            dep.dependencies_for("B").unwrap(),
-            ["C", "E", "F", "G", "H"]
-        );
-        assert_contents_eq!(dep.dependencies_for("C").unwrap(), ["G"]);
-        assert_contents_eq!(
-            dep.dependencies_for("D").unwrap(),
+        // let deps: Vec<_> = dep.dependencies_for("A").unwrap().collect();
+        // assert_ok_contents_eq!(deps, ["B", "C", "E", "F", "G", "H"]);
+        assert_ok_contents_eq!(dep.dependencies_for("A"), ["B", "C", "E", "F", "G", "H"]);
+        assert_ok_contents_eq!(dep.dependencies_for("B"), ["C", "E", "F", "G", "H"]);
+        assert_ok_contents_eq!(dep.dependencies_for("C"), ["G"]);
+        assert_ok_contents_eq!(
+            dep.dependencies_for("D"),
             ["A", "B", "C", "E", "F", "G", "H"]
         );
-        assert_contents_eq!(dep.dependencies_for("E").unwrap(), ["F", "H"]);
-        assert_contents_eq!(dep.dependencies_for("F").unwrap(), ["H"]);
+        assert_ok_contents_eq!(dep.dependencies_for("E"), ["F", "H"]);
+        assert_ok_contents_eq!(dep.dependencies_for("F"), ["H"]);
     }
 
     #[test]
@@ -122,8 +120,8 @@ mod tests {
         dep.add_direct("B", &["C"]);
         dep.add_direct("C", &["A"]);
 
-        assert_contents_eq!(dep.dependencies_for("A").unwrap(), ["B", "C",]);
-        assert_contents_eq!(dep.dependencies_for("B").unwrap(), ["A", "C"]);
-        assert_contents_eq!(dep.dependencies_for("C").unwrap(), ["A", "B"]);
+        assert_ok_contents_eq!(dep.dependencies_for("A"), ["B", "C",]);
+        assert_ok_contents_eq!(dep.dependencies_for("B"), ["A", "C"]);
+        assert_ok_contents_eq!(dep.dependencies_for("C"), ["A", "B"]);
     }
 }
